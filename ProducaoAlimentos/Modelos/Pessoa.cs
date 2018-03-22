73f1e55b134091777ca846e85da39211967df2ea
Marcelo Cezário <marcelocezario@gmail.com>
2018-03-22 00:35:33 -0300

    Criando atributos da classe Pessoa

--- ProducaoAlimentos/Modelos/Pessoa.cs
+++ ProducaoAlimentos/Modelos/Pessoa.cs
@@ -1,6 +1,18 @@
-﻿namespace Modelos
+﻿using System;
+
+namespace Modelos
 {
-    class Pessoa
+    public abstract class Pessoa
     {
+        public int PessoaID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf_Cnpj { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public int EnderecoID { get; set; }
+
     }
 }
